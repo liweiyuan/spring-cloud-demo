225c1d48756f55331151e4677b91d0dec937312c
liweiyuan <535767595@qq.com>
2017-11-10 17:27:47 +0800

    add config-git

---
+++ config-server-git/src/main/java/com/wade/cloud/ConfigServerGitApplication.java
@@ -0,0 +1,12 @@
+package com.wade.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerGitApplication.class, args);
+	}
+}
