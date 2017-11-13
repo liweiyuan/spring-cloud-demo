02ebd0715ced915ee7e03e0d913802a258d844a7
liweiyuan <535767595@qq.com>
2017-11-13 16:54:53 +0800

    add api-gateway 服务网关

---
+++ api-gateway/src/main/java/com/wade/cloud/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.wade.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Spring Cloud Zuul来构建服务网关
+ */
+@EnableZuulProxy   //开启Zuul的功能
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
