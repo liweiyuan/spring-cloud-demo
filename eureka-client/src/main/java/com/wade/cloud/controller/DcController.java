bfa014a4507d2514fe06165796bc4dcf2639cce7
liweiyuan <535767595@qq.com>
2017-11-17 14:16:57 +0800

    add 熔断器的相关配置，主要针对HystrixCommand注解的用法

--- eureka-client/src/main/java/com/wade/cloud/controller/DcController.java
+++ eureka-client/src/main/java/com/wade/cloud/controller/DcController.java
@@ -18,7 +18,7 @@ public class DcController {
     @GetMapping("/dc")
     public String dc(){
         try {
-            Thread.sleep(5000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- eureka-feign-consumer-hystrix/src/main/java/com/wade/cloud/controller/DcController.java
+++ eureka-feign-consumer-hystrix/src/main/java/com/wade/cloud/controller/DcController.java
@@ -1,6 +1,7 @@
 package com.wade.cloud.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.wade.cloud.service.DcClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,20 @@ public class DcController {
     @Autowired
     DcClient dcClient;
 
-    @HystrixCommand(fallbackMethod = "fallback")
+    /**
+     * 表示在10分钟内(60000),如果调取服务超过5秒，调取服务错误个数为1个时，就直接熔断，调取fallback
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "fallback",
+            commandProperties = {@HystrixProperty(name ="execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "1"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "600000")})//快照时间
     @GetMapping("/consumer")
     public String dc(){
-        return dcClient.consumer();
+        System.err.println("--------------------------------------");
+        String result=dcClient.consumer();
+        System.err.println("end**************************");
+        return  result;
     }
 
     public String fallback(){
