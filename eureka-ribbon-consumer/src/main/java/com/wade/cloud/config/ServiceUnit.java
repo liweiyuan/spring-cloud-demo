3312fd27e61974e4199d01eb375b0843ab4f01de
liweiyuan <535767595@qq.com>
2017-11-24 15:18:02 +0800

    add 简单用户微服务-集成solr

--- eureka-ribbon-consumer/src/main/java/com/wade/cloud/config/ServiceUnit.java
+++ eureka-ribbon-consumer/src/main/java/com/wade/cloud/config/ServiceUnit.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.Netty4ClientHttpRequestFactory;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -35,13 +36,25 @@ public class ServiceUnit {
     }*/
 
 
-    @Bean
+    /*@Bean
     @LoadBalanced
     public RestTemplate getNetty4Instance() {
         RestTemplate restTemplate = new RestTemplate();
         Netty4ClientHttpRequestFactory factory = new Netty4ClientHttpRequestFactory();
         factory.setConnectTimeout(5000 * 2);
         factory.setReadTimeout(5000 * 2);
+        restTemplate.setRequestFactory(factory);
+        return restTemplate;
+    }*/
+
+
+    //基于OkHttp3
+    @Bean
+    @LoadBalanced
+    public RestTemplate getOkHttp3Instance(){
+        RestTemplate restTemplate=new RestTemplate();
+        OkHttp3ClientHttpRequestFactory factory=new OkHttp3ClientHttpRequestFactory();
+        //采用默认值
         restTemplate.setRequestFactory(factory);
         return restTemplate;
     }
---
+++ springms-simple-provider-user/src/main/java/com/wade/cloud/SpringmsSimpleProviderUserApplication.java
@@ -0,0 +1,12 @@
+package com.wade.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmsSimpleProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmsSimpleProviderUserApplication.class, args);
+	}
+}
